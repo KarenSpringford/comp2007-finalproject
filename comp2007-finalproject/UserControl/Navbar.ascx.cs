438b2ec1eca6f4903974016355d6c6d9fb94e171
Karen Springford <200299681@student.georgianc.on.ca>
2016-06-12 22:33:25 -0400

    BackEnd Navbar Amendments /File Structure changes

---
+++ comp2007-finalproject/UserControl/Navbar.ascx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp2007_finalproject
+{
+	public partial class Navbar : System.Web.UI.UserControl
+	{
+		protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    Home.Attributes.Add("class", "active");
+                    break;
+                case "Schedule":
+                    Schedule.Attributes.Add("class", "active");
+                    break;
+                case "Stats":
+                    Stats.Attributes.Add("class", "active");
+                    break;
+                case "Teams":
+                    Teams.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    Contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
