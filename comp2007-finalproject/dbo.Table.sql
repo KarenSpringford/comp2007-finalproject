5161951e5f392881695eb64629e6b9cdfe4d1a13
KarenSpringford <jkspringford@gmail.com>
2016-06-17 19:50:16 -0400

    Updated the sql

--- comp2007-finalproject/dbo.Table.sql
+++ comp2007-finalproject/dbo.Table.sql
@@ -8,3 +8,13 @@ CREATE TABLE [dbo].[Teams] (
 
 
 INSERT INTO [dbo].[Teams] VALUES ('Banana')
+
+--Teams
+INSERT INTO Teams VALUES ('GirlScout DropOuts');
+INSERT INTO Teams VALUES ('All Dodge No Balls');
+INSERT INTO Teams VALUES ('DodgieBirds');
+INSERT INTO Teams VALUES ('Gym Class Heroes');
+INSERT INTO Teams VALUES ('Not In the Face');
+INSERT INTO Teams VALUES ('Much Ado About Balls');
+INSERT INTO Teams VALUES ('Ballsagna');
+INSERT INTO Teams VALUES ('Balls & Dolls');
