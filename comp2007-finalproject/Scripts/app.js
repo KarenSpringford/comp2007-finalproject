ced48ccae563d349fb1a78cffa0e071a11aabe85
Aristotle Rovithis <aristotle.rovithis@gmail.com>
2016-06-18 01:42:16 -0400

    added confirm function to all delete buttons

--- comp2007-finalproject/Scripts/app.js
+++ comp2007-finalproject/Scripts/app.js
@@ -2,5 +2,12 @@
  * @fileOverview app.js
  * @author Aristotle Rovithis & Karen Springford
  * @description Custom JavaScript for Sport Tracking app
- * @version 0.0.1
+ * @version 0.0.2
  */
+
+$(document).ready(function () {
+    console.log('app started');
+    $('a.btn.btn-danger.btn-sm').click(function () {
+        return confirm('Are you sure you want to delete?');
+    })
+})
