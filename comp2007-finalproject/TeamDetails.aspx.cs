d22e0ce97173138163a096acb9f4bc202429fe10
KarenSpringford <jkspringford@gmail.com>
2016-06-18 00:29:18 -0400

    Added Calendar to Default.aspx

--- comp2007-finalproject/TeamDetails.aspx.cs
+++ comp2007-finalproject/TeamDetails.aspx.cs
@@ -20,7 +20,40 @@ namespace comp2007_finalproject
     
         protected void SaveButton_Click(object sender, EventArgs e)
         {
+            //connect to the server
+            using (TeamConnection db = new TeamConnection())
+            {
+                Team newTeam = new Team();
 
+                int teamID = 0;
+
+                if (Request.QueryString.Count > 0)
+                {
+                    teamID = Convert.ToInt32(Request.QueryString["teamID"]);
+
+                    newTeam = (from team in db.Teams
+                                  where team.TeamId == teamID
+                                  select team).FirstOrDefault();
+                }
+
+                //add data to the new student record
+                newTeam.TeamName = TeamNameTextBox.Text;
+
+
+
+                if (teamID == 0)
+                {
+                    //use LINQ and ADO.NET to add or insert newStudent into the Database
+                    db.Teams.Add(newTeam);
+                }
+
+
+                //save changes
+                db.SaveChanges();
+
+                //redirect back to the updated students page
+                Response.Redirect("~/Students.aspx");
+            }
         }
 
         protected void CancelButton_Click(object sender, EventArgs e)
